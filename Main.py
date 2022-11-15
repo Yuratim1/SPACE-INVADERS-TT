e68b785b67ae37530ca138720c528f789b1e3de2
Yuratim1 <timoshenkov.y@gmail.com>
2022-11-15 12:05:07 -0800

    added game class + graphics

--- Main.py
+++ Main.py
@@ -5,25 +5,40 @@ import pygame
 import os
 import sys
 
-#Initial Setup
-pygame.init()
+class Game:
+    def __init__(self):
+        pass
 
-screen_width = 400
-screen_height = 400
-fps = 60
+    # update all sprite groups
+    # draw all sprite groups
+    def run(self):
+        pass
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Space Invaders")
-clock = pygame.time.Clock()
+        
+        
+if __name__ == '__main__':
+    #Initial Setup
+    pygame.init()
+
+    screen_width = 600
+    screen_height = 600
+    fps = 60
+
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Space Invaders")
+    clock = pygame.time.Clock()
+    game = Game()
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
     
-    screen.fill((30, 30, 30))
+        screen.fill((30, 30, 30))
+        game.run()
 
-    pygame.display.flip()
-    clock.tick(60)
+
+        pygame.display.flip()
+        clock.tick(60)
