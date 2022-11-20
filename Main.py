ab73a275971987f383419adbd2f4de910494786a
Yuratim1 <timoshenkov.y@gmail.com>
2022-11-20 10:36:34 -0800

    aliens feature finished

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ import pygame
 import sys
 from Player import Player
 import obstacles
-import aliens
+from aliens import Aliens, Bonusalien
+from random import choice, randint
+from laser import Laser
 
 
 class Game:
@@ -10,6 +12,7 @@ class Game:
         player_sprite = Player((SCREEN_WIDTH / 2, SCREEN_HEIGHT), SCREEN_WIDTH, 5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
 
+        #block setup
         self.shape = obstacles.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
@@ -17,6 +20,18 @@ class Game:
         self.obstacle_x_positions = [int(num * (SCREEN_WIDTH / self.obstacle_amount)) for num in range(self.obstacle_amount)]
         print(self.obstacle_x_positions)
         self.create_mult_obstacles(self.obstacle_x_positions , x_start = SCREEN_WIDTH / 14, y_start = 480)
+
+        #Alien setup
+        self.aliens = pygame.sprite.Group()
+        self.alien_lasers = pygame.sprite.Group()
+        self.aliens_setup( rows = 6, cols = 8)
+        self.alien_direction = 1
+        
+        #bonus alien
+        self.bonus_alien = pygame.sprite.GroupSingle()
+        self.bonus_alien_spawn_time = randint(400, 800)
+        
+
 
     def create_obstacles(self, x_start, y_start, offset_x):
         for row_index, row in enumerate(self.shape):
@@ -31,14 +46,65 @@ class Game:
         for offset_x in args:
             self.create_obstacles(x_start, y_start, offset_x)
 
+    def aliens_setup(self, rows, cols, x_distance = 60, y_distance = 48, x_offset = 70, y_offset = 70):
+        for row_index, row in enumerate(range(rows)):
+            for col_index, col in enumerate(range(cols)):
+                x = col_index * x_distance + x_offset
+                y = row_index * y_distance + y_offset
+                
+                if row_index == 0: alien_sprite = Aliens('yellow', x, y)
+                elif 1 <= row_index <= 2: alien_sprite = Aliens('green', x, y)
+                else: alien_sprite = Aliens('red', x, y)
+                self.aliens.add(alien_sprite)
+
+    def alien_pos_checker(self):
+        all_aliens = self.aliens.sprites()
+        for alien in all_aliens:
+            if alien.rect.right >= SCREEN_WIDTH:
+                self.alien_direction = -1
+                self.alien_move_down(2)
+            elif alien.rect.left <= 0:
+                self.alien_direction = 1
+                self.alien_move_down(2)
+    
+    def alien_move_down(self, distance):
+        if self.aliens:  
+            for alien in self.aliens.sprites():
+                alien.rect.y += distance
+    
+    def bonus_alien_time(self):
+        self.bonus_alien_spawn_time -= 1
+        if self.bonus_alien_spawn_time <= 0:
+            self.bonus_alien.add(Bonusalien(choice(['right', 'left']), SCREEN_WIDTH))
+            self.bonus_alien_spawn_time = randint(400,800)
+    
+    def alien_shoot(self):
+        if self.aliens.sprites():
+            random_alien = choice(self.aliens.sprites())
+            laser_sprite = Laser(random_alien.rect.center, -6, SCREEN_HEIGHT)
+            self.alien_lasers.add(laser_sprite)
+            
+    def collison_checks(self):
+        
+    
+    
     # update all sprite groups
     # draw all sprite groups
     def run(self):
         self.player.update()
+        
+        self.aliens.update(self.alien_direction)
+        self.alien_pos_checker()
+        self.alien_lasers.update()
+        self.bonus_alien_time()
+        self.bonus_alien.update()
 
         self.player.sprite.bullet.draw(screen)
         self.player.draw(screen)
         self.blocks.draw(screen)
+        self.aliens.draw(screen)
+        self.alien_lasers.draw(screen)
+        self.bonus_alien.draw(screen)
         
         
         
@@ -49,6 +115,9 @@ if __name__ == '__main__':
     SCREEN_WIDTH = 600
     SCREEN_HEIGHT = 600
     FPS = 60
+    
+    ALIENLASER = pygame.USEREVENT + 1
+    pygame.time.set_timer(ALIENLASER, 800)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Space Invaders")
@@ -61,6 +130,8 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == ALIENLASER:
+                game.alien_shoot()
     
         screen.fill((30, 30, 30))
         game.run()
